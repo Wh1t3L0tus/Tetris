5a6d27fd8d5eb2fe8dd52bf62b2e37699453edf0
Wh1t3L0tus <wh1t3l07u5@gmail.com>
2012-08-21 13:47:43 +0200

    Added constructors and the expected methods for the Block class

--- src/Block.java
+++ src/Block.java
@@ -6,5 +6,27 @@ public class Block {
 	private Image texture;
 	private boolean blank;
 	
+	public Block(boolean blank, Image texture) {
+		this.blank = blank;
+		this.texture = texture;
+	}
+	
+	public Block(boolean blank) {
+		this.texture = null;
+		this.blank = blank;
+	}
+	
+	public boolean isBlank() {
+		return blank;
+	}
+	
+	public void setTexture(Image texture) {
+		this.texture = texture;
+	}
+	
+	public void render(float x, float y) {
+		texture.draw(x, y);
+	}
+	
 	
 }
