6863675113a814c2fac044fc5459f99338249a96
GreenFenrir <greenfenrir@gmail.com>
2012-08-23 20:08:17 +0200

    Completed the Board class except for the erase line method

--- src/Board.java
+++ src/Board.java
@@ -4,20 +4,45 @@ import java.util.ArrayList;
 public class Board {
 	
 	private ArrayList<Block> matrix;
+	private Tetrimino tetrimino;
 	private int width;
 	private int height;
 	
-	
-	public int getHeight() {
-		return height;
+	public Board(int width, int height) {
+		this.height = height+5;
+		this.width = width+2;
+		this.matrix = new ArrayList<Block> (this.width*this.height);
+		/*+2 pour les bordure à droite et à gauche de la matrice, +5 pour la bordure en bas et les quatres lignes en haut où apparetront les tetrimino*/
 	}
 	
-	public int getWidth() {
-		return width;
+	public Board() {
+		this(10,22);
 	}
 	
 	public ArrayList<Block> getMatrix() {
 		return matrix;
 	}
-
+	public void setMatrix(ArrayList<Block> matrix) {
+		this.matrix = matrix;
+	}
+	public Tetrimino getTetrimino() {
+		return tetrimino;
+	}
+	public void setTetrimino(Tetrimino tetrimino) {
+		this.tetrimino = tetrimino;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	
 }
