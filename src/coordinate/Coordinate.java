782dff2cb034a13c7b3356f46167df003722f0cf
GreenFenrir <greenfenrir@gmail.com>
2012-08-24 16:43:41 +0200

    Added getters and setters in the Coordinate Class

--- src/coordinate/Coordinate.java
+++ src/coordinate/Coordinate.java
@@ -16,4 +16,20 @@ public class Coordinate {
 	public Coordinate(Coordinate c) {
 		this(c.x, c.y);
 	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
 }
