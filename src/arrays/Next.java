51b740a76bede245761d503b2e6fa06cad619866
GreenFenrir <greenfenrir@gmail.com>
2012-08-24 16:49:21 +0200

    Started the Next class

--- src/arrays/Next.java
+++ src/arrays/Next.java
@@ -1,5 +1,28 @@
 package arrays;
 
-public class Next {
+import java.util.ArrayList;
+import tetrimino.*;
+import block.Block;
 
+public class Next {
+	
+	private ArrayList<Block> matrix;
+	private Tetrimino tetrimino;
+	
+	public Next() {
+		this.matrix = new ArrayList<Block> (4*4);
+	}
+	
+	public ArrayList<Block> getMatrix() {
+		return matrix;
+	}
+	public void setMatrix(ArrayList<Block> matrix) {
+		this.matrix = matrix;
+	}
+	public Tetrimino getTetrimino() {
+		return tetrimino;
+	}
+	public void setTetrimino(Tetrimino tetrimino) {
+		this.tetrimino = tetrimino;
+	}
 }
