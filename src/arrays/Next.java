7b8d76b8a6a64b23268866913fd15eea18d0ac6f
Wh1t3L0tus <wh1t3l07u5@gmail.com>
2012-09-16 13:14:47 +0200

    Added the generateTetrimino() method in the Next class (and updated the constructor)

--- src/arrays/Next.java
+++ src/arrays/Next.java
@@ -13,6 +13,9 @@ public class Next {
 	public Next() {
 		this.matrix = new Block[4*4];
 		this.coordinate = new Coordinate();
+		for (int i = 0; i < matrix.length; i++)
+			this.matrix[i] = new Block(true);
+		generateTetrimino();
 	}
 	
 	public Block[] getMatrix() {
@@ -34,6 +37,34 @@ public class Next {
 	
 	public Coordinate getCoordinate() {
 		return coordinate;
+	}
+	
+	public Tetrimino generateTetrimino() {
+		int n = (int) Math.random() * 7;
+		Tetrimino t = null;
+		Tetrimino toReturn = tetrimino;
+		if (n == 0)
+			t = new ITetrimino();
+		else if (n == 1)
+			t = new JTetrimino();
+		else if (n == 2)
+			t = new LTetrimino();
+		else if (n == 3)
+			t = new OTetrimino();
+		else if (n == 4)
+			t = new STetrimino();
+		else if (n == 5)
+			t = new TTetrimino();
+		else
+			t = new ZTetrimino();
+		
+		tetrimino = t;
+		for (int i = 0; i < tetrimino.getBlockList().length; i++) {
+			Coordinate c = tetrimino.getBlockList()[i];
+			matrix[c.y * 4 + c.x].setTexture(tetrimino.getTexture());
+		}
+		return toReturn;
+		
 	}
 	
 	public void render() {
